2be562fbe31f65eb13a59b72892760dbd8a38273
Max Mellor <mellormax1@gmail.com>
2023-09-04 16:24:33 +0100

    Reverted dashboard title

--- src/scenes/dashboard/index.jsx
+++ src/scenes/dashboard/index.jsx
@@ -76,7 +76,7 @@ class Dashboard extends Component {
             <Box m="20px">
                 {this.state.isLoading && <LoadingCircle/>}
                 <Box display="flex" justifyContent="space-between" alignItems="center">
-                    <Header title="DASHBOARD Test" subtitle="Welcome to your dashboard"/>
+                    <Header title="DASHBOARD" subtitle="Welcome to your dashboard"/>
                     <Typography variant="h3" fontWeight="bold">{monthIndexToString(this.state.selectedMonth)} {this.state.selectedYear}</Typography>
                 </Box>
                 {/* GRID AND CHARTS */}
